2c5e079faf342bab88986f16bbd8a90c740ff758
Ana Karen Lopez Carbajal <67447283+aklopezcarbajal@users.noreply.github.com>
2021-03-04 13:58:46 -0500

    Added resource class and parser

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str, required=True)
+parser.add_argument('species', type=str, required=True)
+parser.add_argument('personality', type=str, required=True)
+parser.add_argument('quote', type=str, required=True)
+
+class villager(Resource):
+	def get(self):
+		return ''
+
+	def put(self, name):
+		return ''
+
+api.add_resource(villager, '/<string:name>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
